25bedee78c5ce4982a0c1ba32b2a1742e1bc4b24
xuyi12 <xuyi12@jd.com>
2019-03-16 16:56:57 +0800

    outputManagement

--- outputManagement/webpack.config.js
+++ outputManagement/webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugin:[
+        new HtmlWebpackPlugin()
+    ]
 }
